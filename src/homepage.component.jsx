eb5f0c6c5131bd63dd46327e5a417cbc6e4225e9
Anuj Bhartia <anujbhartia96@gmail.com>
2021-04-11 23:40:55 +0530

    created project with React and Scss

---
+++ src/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+export const HomePage = (props) => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">1</h1>
+          <span className="subtitle"></span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
