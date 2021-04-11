b9245280fc6e72a01ea52847ad727eee0ff0ffd3
Anuj Bhartia <anujbhartia96@gmail.com>
2021-04-12 01:11:09 +0530

    broken into small components and homepage setup

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "./../../components/directory/directory-menu.components";
+export const HomePage = (props) => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
