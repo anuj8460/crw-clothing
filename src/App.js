e97ce507660a01ffb54431c5ca27647fc8137534
Anuj Bhartia <anujbhartia96@gmail.com>
2021-04-12 23:57:11 +0530

    add shop page, collcetion preview and item

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React from "react";
 import "./App.css";
 import { HomePage } from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
-
+import ShopPage from "./pages/shop/shop.componet";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Switch>
-          <Route exact path="/shop/hats" component={HatsPage} />
+          <Route exact path="/shop" component={ShopPage} />
           <Route exact path="/" component={HomePage} />
         </Switch>
       </header>
