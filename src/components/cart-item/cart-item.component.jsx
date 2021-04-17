223c9fd077f9ea00cd37c212cf02ab4193f0c80d
Anuj Bhartia <anujbhartia96@gmail.com>
2021-04-18 02:54:03 +0530

    updated code using reselect in cart-dropdown and cart-icon

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -5,7 +5,7 @@ const CartItem = ({ item: { name, price, imageUrl, quantity } }) => (
   <div className="cart-item">
     <img src={imageUrl} alt="item" />
     <div className="item-details">
-      <span className="name"></span>
+      <span className="name">{name}</span>
       <span className="price">
         {quantity} x ${price}
       </span>
